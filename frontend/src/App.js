ad4fe18015cc8736c7ef90ee7472ea5da87a25e8
DanielBarda97 <danielbarda97@gmail.com>
2021-06-19 13:03:57 +0300

    Product Search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,6 +41,7 @@ const App = () => {
           <Route path="/admin/productslist" component={ProductListScreen} />
           <Route path="/admin/orderslist" component={OrderListScreen} />
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
+          <Route path="/search/:keyword" component={HomeScreen} />
           <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
